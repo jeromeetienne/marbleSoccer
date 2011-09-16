2867ed473171a7759e60aeff535da00faf65f056
Jerome Etienne <jerome.etienne@gmail.com>
2011-09-16 18:17:45 +0200

    massive refactoring + usable camera + better enemy move

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 // declare a bunch of variable we will need later
 var startTime	= Date.now();
 var container;
-var keyboard, player, enemy, map;
+var keyboard, world;
 var camera, scene, renderer, stats;
 var skyboxMesh;
 
@@ -23,11 +23,6 @@ if ( ! Detector.webgl ){
 
 // ## Initialize everything
 function init() {
-	// create the camera
-	camera = new THREE.Camera( 60, window.innerWidth / window.innerHeight, 1, 100000 );
-	camera.position.y	= 100;
-	camera.position.z	= 500;
-
 	// create the Scene
 	scene = new THREE.Scene();
 	
@@ -40,23 +35,10 @@ function init() {
 	var directionalLight = new THREE.DirectionalLight( 0xffffff );
 	directionalLight.position.set( 0, 0, 1 ).normalize();
 	scene.addLight( directionalLight );
-				
-	if( true ){
-		var skybox	= new Marble.Skymap();
-		skyboxMesh	= skybox.mesh();
-		scene.addObject( skyboxMesh );
-	}
-
-
-	map		= new Marble.Map();
-	scene.addObject( map.mesh() );
-
-	player		= new Marble.Player();
-	scene.addObject( player.mesh() );
-
-	enemy	= new Marble.Enemy();
-	enemy.mesh().position.x	= 30;
-	scene.addObject( enemy.mesh() );
+	
+	world	= new Marble.World({
+		scene	: scene
+	});
 
 	// ## End of the Skybox Code
 
@@ -71,7 +53,7 @@ function init() {
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	container.appendChild( renderer.domElement );
 	
-	THREEx.WindowResize(renderer, camera);
+	THREEx.WindowResize(renderer, world.camera().object());
 	keyboard	= new THREEx.KeyboardState();
 
 	// init the Stats and append it to the Dom - performance vuemeter
@@ -95,32 +77,9 @@ function animate() {
 // ## Render the 3D Scene
 function render() {
 
-	player.tick(); 
-	map.tick();
+	// world .tick()
+	world.tick();
 	
-(function(){
-	var player2Enemy	= new THREE.Vector3();
-	player2Enemy.sub( enemy.mesh().position, player.mesh().position);
-
-	vector	= player2Enemy.normalize().negate().multiplyScalar(Marble.tileSize);
-
-	camera.position.add( player.mesh().position, vector );
-	camera.position.y	= Marble.tileSize;
-
-	camera.position.y	= Marble.tileSize;
-
-	camera.target.position	= enemy.mesh().position.clone();
-}());
-
-	// make the camera follow the player
-	if( false ){
-		camera.position.x = player.mesh().position.x;
-		camera.position.y = player.mesh().position.y + 100;
-		camera.position.z = player.mesh().position.z + 500;
-		
-		camera.target.position	= player.mesh().position;
-	}
-
 	// actually display the scene in the Dom element
-	renderer.render( scene, camera );
+	renderer.render( scene, world.camera().object() );
 }
