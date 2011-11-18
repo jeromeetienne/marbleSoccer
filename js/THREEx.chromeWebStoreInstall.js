9a9906b28284228747d7deac1d2815fec2faf912
Jerome Etienne <jerome.etienne@gmail.com>
2011-11-18 17:12:00 +0100

    js/THREEx.chromeWebStoreInstall.js better comment

--- js/THREEx.chromeWebStoreInstall.js
+++ js/THREEx.chromeWebStoreInstall.js
@@ -11,18 +11,33 @@
 var THREEx			= THREEx 			|| {};
 THREEx.ChromeWebStoreInstall	= THREEx.ChromeWebStoreInstall	|| {};
 
+/**
+ * test if the API is available
+ * @returns {Boolean} true if the API is available, false otherwise
+*/
 THREEx.ChromeWebStoreInstall.apiAvailable	= function()
 {
 	var available	= typeof chrome !== 'undefined' && chrome.webstore && chrome.webstore.install;
 	return available ? true : false;
 }
 
+/**
+ * Test if the application is already installed
+ * 
+ * @returns {Boolean} true if the application is installed, false otherwise
+*/
 THREEx.ChromeWebStoreInstall.isInstalled	= function()
 {
 	if( !this.apiAvailable() )	return false;
 	return chrome.app.isInstalled ? true : false;
 }
 
+/**
+ * Trigger an installation
+ * @param {String} url of the application (optional)
+ * @param {Function} callback called if installation succeed
+ * @param {Function} callback called if installation failed
+*/
 THREEx.ChromeWebStoreInstall.install	= function(url, successCallback, failureCallback)
 {
 	console.assert( this.apiAvailable() )
